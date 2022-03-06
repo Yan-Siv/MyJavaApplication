6068d582d8ef042b9cb023e0a3acfef4ea3d9f55
YanSivak <yan.sivak@yandex.ru>
2022-03-06 18:02:41 +0400

    testing

---
+++ src/main/java/com/lisenok/testYanApp/printlol.java
@@ -0,0 +1,11 @@
+package com.lisenok.testYanApp;
+
+/**
+ *
+ * @author yansi
+ */
+public class printlol {
+    public static void main(String[] args){
+        System.out.println("121");
+    }
+}
