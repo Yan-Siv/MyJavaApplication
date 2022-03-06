14f6a3c7bb6a623763872d07a3195a2fb943f90e
YanSivak <>
2022-03-06 17:45:28 +0400

    testing

---
+++ src/main/java/com/lisenok/testYanApp/Mavenproject1.java
@@ -0,0 +1,16 @@
+
+
+package com.lisenok.testYanApp;
+
+/**
+ *
+ * @author YanSivak aka Lisenok
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        printlol.
+//    System.out.println("121");
+    }
+}
